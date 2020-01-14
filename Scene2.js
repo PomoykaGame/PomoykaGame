8ff5e2807f6a84183514df1440cb1dd7e5f33bb6
Eduard Palchevskiy <Eduard>
2020-01-14 23:02:39 +0200

    sosit

--- Scene2.js
+++ Scene2.js
@@ -4,13 +4,49 @@ class Scene2 extends Phaser.Scene {
   }
 
   create() {
-  this.player = this.physics.add.sprite(100, 200, 'dude');
-  this.player.body.setGravity(0, 600);
-  this.player.setBounce(0);
-  this.player.setCollideWorldBounds(true);
+    this.player = this.physics.add.sprite(100, 200, 'dude');
+    this.player.body.setGravityY(1000);
+    this.player.setBounce(0);
+    this.player.setCollideWorldBounds(true);
+
+    this.playerJumped = false;
+    this.i = 0;
+
   }
 
-  update(){
 
+  update() {
+    this.cursors = this.input.keyboard.createCursorKeys();
+    if (this.cursors.up.isDown && this.player.body.onFloor()) {
+      this.i = 0;
+      this.playerJumped = true;
+      this.player.setVelocityY(-530);
+    }
+
+    else if (this.cursors.up.isDown && this.playerJumped === true) {
+      this.i++;
+      if (this.i > 40) {
+        this.playerJumped = false;
+      }
+      this.player.body.setGravityY(1000);
+    }
+
+    else {
+      this.playerJumped = false;
+      this.player.body.setGravityY(2000);
+    }
+     
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+    }
+
+    else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+    }
+
+    else {
+      this.player.setVelocityX(0);
+    }
+  
   }
 }
